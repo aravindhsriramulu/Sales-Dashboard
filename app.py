b95e8a6ed63188cba9c5481a6667b39c46b4a4f1
aravindhsriramulu <108921057+aravindhsriramulu@users.noreply.github.com>
2023-02-23 11:13:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title = 'Sales Dashboard',
 
 @st.cache
 def get_data():
-    df = pd.read_csv('data/data.csv')
+    df = pd.read_csv('data/erpdata.csv')
     df['date']= pd.to_datetime(df['Period'])
     return df
 
@@ -49,7 +49,7 @@ total1,total2,total3,total4,total5,total6,total7 = st.columns(5,gap='large')
 
 with total1:
     st.image('images/impression.png',use_column_width='Auto')
-    st.metric(label = 'Total Volume', value= numerize(total_volume))
+    st.metric(label='Total Volume', value= numerize(total_volume))
     
 with total2:
     st.image('images/tap.png',use_column_width='Auto')
@@ -79,44 +79,44 @@ Q1,Q2 = st.columns(2)
 
 with Q1:
     df3 = df1.groupby(by = ['Pack']).sum()[['Net Sales','Cost of Goods Sold']].reset_index()
-    df3['Gross Profit'] =round(df3['Net Sales']/df3['Cost of Goods Sold'] *100,3)
-    fig_CTR_by_campaign = px.bar(df3,
+    df3['Gross Profit'] =round(df3['Net Sales'] + df3['Cost of Goods Sold'],3)
+    fig_profit_by_volume = px.bar(df3,
                             x='Pack',
                             y='Gross Profit',
                             title='<b>How pack volume impact profit</b>')
-    fig_CTR_by_campaign.update_layout(title = {'x' : 0.5},
+    fig_profit_by_volume.update_layout(title = {'x' : 0.5},
                                     plot_bgcolor = "rgba(0,0,0,0)",
                                     xaxis =(dict(showgrid = False)),
                                     yaxis =(dict(showgrid = False)))
-    st.plotly_chart(fig_CTR_by_campaign,use_container_width=True)
+    st.plotly_chart(fig_profit_by_volume,use_container_width=True)
 
 with Q2:
-    fig_impressions_per_day = px.line(df1,x='date',
-                                    y=['Distribution'],
-                                    color='Warehousing',
-                                    title='<b>How much goods are distributed vs stored</b>')
-    fig_impressions_per_day.update_xaxes(rangeslider_visible=True)
-    fig_impressions_per_day.update_layout(xaxis_range=['2021-01-01','2021-01-31'],
+    fig_sales_per_day = px.line(df1,x='date',
+                                    y=['Net Sales'],
+                                    color='Discounts',
+                                    title='<b>Sales figure for the past two years</b>')
+    fig_sales_per_day.update_xaxes(rangeslider_visible=True)
+    fig_sales_per_day.update_layout(xaxis_range=['2015-01-01','2016-12-31'],
                                         showlegend = False,
                                         title = {'x' : 0.5},
-                                         plot_bgcolor = "rgba(0,0,0,0)",
+                                        plot_bgcolor = "rgba(0,0,0,0)",
                                         xaxis =(dict(showgrid = False)),
                                         yaxis =(dict(showgrid = False)),)
-    st.plotly_chart(fig_impressions_per_day,use_container_width=True)
+    st.plotly_chart(fig_sales_per_day,use_container_width=True)
   
 Q3,Q4 = st.columns(2)
 
 with Q3:
     df4 = df1.groupby(by='Client Type').sum()[['Volume']].reset_index()
-    fig_spend_by_gender = px.pie(df4,names='Client Type',values='Volume',title='<b>Client Type Volume Stats</b>')
-    fig_spend_by_gender.update_layout(title = {'x':0.5}, plot_bgcolor = "rgba(0,0,0,0)")
-    st.plotly_chart(fig_spend_by_gender,use_container_width=True)
+    fig_diff_client_type = px.pie(df4,names='Client Type',values='Volume',title='<b>Client Type Volume Stats</b>')
+    fig_diff_client_type.update_layout(title = {'x':0.5}, plot_bgcolor = "rgba(0,0,0,0)")
+    st.plotly_chart(fig_diff_client_type,use_container_width=True)
 
 with Q4:
-    df5 = df1.groupby(by='Size').sum()[['Spent','Total_Conversion']].reset_index()
-    df5['Full Delivered Margin'] = round(df5['Spent']/df5['Total_Conversion'],2)
-    fig_CPC_by_age = px.bar(df5,x = 'age',y='CPC',title='<b>Cost Per Conversion By Age Demographic</b>')
-    fig_CPC_by_age.update_layout(title = {'x':0.5},xaxis =(dict(showgrid = False)),yaxis =(dict(showgrid = False)), plot_bgcolor = "rgba(0,0,0,0)")
+    df5 = df1.groupby(by='Size').sum()[['Net Sales','Cost of Goods Sold', 'Distribution', 'Warehousing']].reset_index()
+    df5['Fully Delivered Margin'] = round(df5['Net Sales] + df5['Cost of Goods Sold'] + df5['Distribution'] + df5['Warehousing'],2)
+    fig_FDM_by_size = px.bar(df5,x = 'size',y='Fully Delivered Margin',title='<b>How FDM changes according to size</b>')
+    fig_FDM_by_size.update_layout(title = {'x':0.5},xaxis =(dict(showgrid = False)),yaxis =(dict(showgrid = False)), plot_bgcolor = "rgba(0,0,0,0)")
     st.plotly_chart(fig_CPC_by_age,use_container_width=True)
 
 
