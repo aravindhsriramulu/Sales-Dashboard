420bd23ae9b0826f19ba57b0569323c2c13f68f9
aravindhsriramulu <108921057+aravindhsriramulu@users.noreply.github.com>
2023-02-26 17:20:11 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+Contents of ~/Pages/app.py
+import streamlit as st
+
+st.markdown("# app 🎈")
+st.sidebar.markdown("# app 🎈")
+
+Contents of ~/Pages/cluster.py
+import streamlit as st
+
+st.markdown("# cluster ❄️")
+st.sidebar.markdown("# cluster ❄️")
