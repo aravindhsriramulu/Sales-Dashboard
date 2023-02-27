b4419b0380a2a710d8355d5dd21a149406d89cea
aravindhsriramulu <108921057+aravindhsriramulu@users.noreply.github.com>
2023-02-27 11:33:28 +0530

    Update cluster.py

--- Pages/cluster.py
+++ Pages/cluster.py
@@ -298,7 +298,7 @@ with tab1:
     
     st.header('Pack vs Profit Distribution')
     st.plotly_chart(fig_packzero)
-    st.text("4X and 15X achieve a gross profit of over $200K. Contrastingly, 6X and 20X attain the lowest profit figure of $60K")
+    st.write("4X and 15X achieve a gross profit of over $200K. Contrastingly, 6X and 20X attain the lowest profit figure of \$60K")
 
     st.header('Size vs Profit Distribution')
     st.plotly_chart(fig_sizezero)
@@ -306,93 +306,93 @@ with tab1:
     
     st.header('Brand vs Profit Distribution')
     st.plotly_chart(fig_brandzero)
-    st.text("Zumba Orange and Pit Bull are the most profitable brands, with a gross profit of $500K and $390K respectively while its subsidiaries does not even cross the $200K threshold. Evan Vitamin does not even manage to hit $10K gross profit")
+    st.write("Zumba Orange and Pit Bull are the most profitable brands, with a gross profit of \$500K and \$390K respectively while its subsidiaries does not even cross the \$200K threshold. Evan Vitamin does not even manage to hit \$10K gross profit")
 
     st.header('Client vs Profit Distribution')
     st.plotly_chart(fig_clientzero)
-    st.text("ShopRite is the most profitable client with a profit mark of $200K while Publix is the least profitable client with a profit mark of less than $20K")
+    st.write("ShopRite is the most profitable client with a profit mark of \$200K while Publix is the least profitable client with a profit mark of less than \$20K")
     
 with tab2:
     st.header('Client Type Distribution')
     st.plotly_chart(fig_typeone)
-    st.text("Supermarkets is the leader with a gross profit of over $5.8M while Discounters, Grocery and Big-box have $2.6M, $1.8M and $1M")
+    st.write("Supermarkets is the leader with a gross profit of over \$5.8M while Discounters, Grocery and Big-box have \$2.6M, \$1.8M and \$1M")
     
     st.header('Pack vs Profit Distribution')
     st.plotly_chart(fig_packone)
-    st.text("All packs have an average gross profit of $500K while the topmost packs are 12X and 15X with a gross profit of $2.4M and $1.4M")
+    st.write("All packs have an average gross profit of \$500K while the topmost packs are 12X and 15X with a gross profit of \$2.4M and \$1.4M")
 
     st.header('Size vs Profit Distribution')
-    st.text("500ML is the highest profitable category with a gross profit if over $4M while other sizes like 250ML, 750ML and 600ML cross the $1.5M threshold. Only 1.25L size range has the least profitable mark threshold of $1M")
     st.plotly_chart(fig_sizeone)
+    st.write("500ML is the highest profitable category with a gross profit if over \$4M while other sizes like 250ML, 750ML and 600ML cross the \$1.5M threshold. Only 1.25L size range has the least profitable mark threshold of \$1M")
     
     st.header('Brand vs Profit Distribution')
     st.plotly_chart(fig_brandone)
-    st.text("Dundy and its subsidiaries is the only profitable brand with an accumulated gross profit of over $8M while Crocky, Evan Vitamin, Buratino and its subsidiaries are under the $500K mark")
+    st.write("Dundy and its subsidiaries is the only profitable brand with an accumulated gross profit of over \$8M while Crocky, Evan Vitamin, Buratino and its subsidiaries are under the \$500K mark")
     
     st.header('Client vs Profit Distribution')
     st.plotly_chart(fig_clientone)
-    st.text("Super Target, Trader Joe's, ShopRite, WinCo Foods, Walmart - Supermarkets cross the $1.5M threshold while Sam's Club, Safeway and Walmart Dicounters have their gross profit below $1M.")
+    st.write("Super Target, Trader Joe's, ShopRite, WinCo Foods, Walmart - Supermarkets cross the \$1.5M threshold while Sam's Club, Safeway and Walmart Dicounters have their gross profit below \$1M.")
 
 with tab3:
     st.header('Client Type Distribution')
     st.plotly_chart(fig_typetwo)
-    st.text("Supermarkets have over $2M gross profit while Big-box and discounters have over $600K")
+    st.write("Supermarkets have over $2M gross profit while Big-box and discounters have over \$600K")
     
     st.header('Pack vs Profit Distribution')
     st.plotly_chart(fig_packtwo)
-    st.text("18X and 20X are the least profitable packs with a gross profit of under $150K while other packs are above $400K")
+    st.write("18X and 20X are the least profitable packs with a gross profit of under \$150K while other packs are above \$400K")
 
     st.header('Size vs Profit Distribution')
     st.plotly_chart(fig_sizetwo)
-    st.text("The most profitable size is 250ML with a gross profit of over $700k and the least profitable size is 750ML with a gross profit of $100K")
+    st.write("The most profitable size is 250ML with a gross profit of over \$700k and the least profitable size is 750ML with a gross profit of \$100K")
     
     st.header('Brand vs Profit Distribution')
     st.plotly_chart(fig_brandtwo)
-    st.text("Evan and Dundy One is the most profitable brand with a gross profit of over $800K")
+    st.write("Evan and Dundy One is the most profitable brand with a gross profit of over \$800K")
     
     st.header('Client vs Profit Distribution')
     st.plotly_chart(fig_clienttwo)
-    st.text("Safeway is the least profitable client with a profit range below $100K")
+    st.write("Safeway is the least profitable client with a profit range below \$100K")
 
 with tab4:
     st.header('Client Type Distribution')
     st.plotly_chart(fig_typethree)
-    st.text("Supermarket has a profit share of almost $8M while Discounters and Big-box of $4M and $3M")
+    st.write("Supermarket has a profit share of almost \$8M while Discounters and Big-box of \$4M and \$3M")
     
     st.header('Pack vs Profit Distribution')
     st.plotly_chart(fig_packthree)
-    st.text("18X and 20X are the least profitable packs with a gross profit range below $1M while other size are above $1M")
+    st.write("18X and 20X are the least profitable packs with a gross profit range below \$1M while other size are above \$1M")
 
     st.header('Size vs Profit Distribution')
     st.plotly_chart(fig_sizethree)
-    st.text("2.25L, 1.75L and 500ML have a profit range of over $2M while 250ML, 750ML and 1.25L have gross profit below $1.5M")
+    st.write("2.25L, 1.75L and 500ML have a profit range of over \$2M while 250ML, 750ML and 1.25L have gross profit below \$1.5M")
     
     st.header('Brand vs Profit Distribution')
     st.plotly_chart(fig_brandthree)
-    st.text("Dundy Diet, Dundy Max and Dundy Free have a gross profit over $3M")
+    st.write("Dundy Diet, Dundy Max and Dundy Free have a gross profit over \$3M")
     
     st.header('Client Vs Distribution')
     st.plotly_chart(fig_clientthree)
-    st.text("Kmart super center is the leading client with a gross profit of over $4M")
+    st.write("Kmart super center is the leading client with a gross profit of over \$4M")
 
 with tab5:
     st.header('Client Type Distribution')
     st.plotly_chart(fig_typefour)
-    st.text("Supermarkets have over $1.8M profit share while Discounters, Grocery and Big-box has over $3.6M, $2.9M and $1.5M")
+    st.write("Supermarkets have over \$1.8M profit share while Discounters, Grocery and Big-box has over \$3.6M, \$2.9M and \$1.5M")
     
     st.header('Pack vs Profit Distribution')
     st.plotly_chart(fig_packfour)
-    st.text("4X and 6X is the most profitable pack, with an average gross profit margin of above $2.5M")
+    st.write("4X and 6X is the most profitable pack, with an average gross profit margin of above \$2.5M")
 
     st.header('Size vs Profit Distribution')
     st.plotly_chart(fig_sizefour)
-    st.text("2.25L and 1.5L are the most profitable size with $5M and $4m respectively")
+    st.write("2.25L and 1.5L are the most profitable size with \$5M and \$4m respectively")
     
     st.header('Brand vs Profit Distribution')
     st.plotly_chart(fig_brandfour)
-    st.text("Dundy and its subsidiaries have a gross profit of over $10M while Evan Vitamin is the least profitable brand with gross profit below $50K")
+    st.write("Dundy and its subsidiaries have a gross profit of over \$10M while Evan Vitamin is the least profitable brand with gross profit below \$50K")
     
     st.header('Client vs Profit Distribution')
     st.plotly_chart(fig_clientfour)
-    st.text("Super Target and Trader's Joe continue to dominate the market with a gross profit of over $2.5M independently")
+    st.write("Super Target and Trader's Joe continue to dominate the market with a gross profit of over \$2.5M independently")
 
